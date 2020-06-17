d490758c270c4d5499ff152e9f870ba7860d1171
Puyan Lotfi <plotfi@fb.com>
2020-06-16 22:11:40 -0700

    Initial Commit

---
+++ SwiftTools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTools
+//
+//  Created by Puyan Lotfi on 6/16/20.
+//  Copyright © 2020 Puyan Lotfi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
