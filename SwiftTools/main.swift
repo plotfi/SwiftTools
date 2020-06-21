755c9985895cfe613c7c942d6deae13b183d1bee
Puyan Lotfi <plotfi@fb.com>
2020-06-20 17:14:43 -0700

    First attempt at a swift shell.

--- SwiftTools/main.swift
+++ SwiftTools/main.swift
@@ -10,3 +10,56 @@ import Foundation
 
 print("Hello, World!")
 
+let envTask = Process()
+envTask.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+let envOutputPipe = Pipe()
+let envErrorPipe = Pipe()
+envTask.standardOutput = envOutputPipe
+envTask.standardError = envErrorPipe
+try envTask.run()
+
+print("ENV:")
+let envOutputData = envOutputPipe.fileHandleForReading.readDataToEndOfFile()
+let envOutput = String(decoding: envOutputData, as: UTF8.self)
+let envOutputByLine = envOutput.split(separator: "\n")
+for line in envOutputByLine {
+    if line.starts(with: "PATH") {
+        print("PATHPATHPATH")
+        print(line)
+    }
+}
+
+while true {
+    print("> ", terminator: "")
+    let cmdOpt = readLine(strippingNewline: true)
+    if cmdOpt == nil {
+        continue;
+    }
+    
+    var cmd = cmdOpt!
+    cmd = cmd.trimmingCharacters(in: NSCharacterSet.whitespaces)
+
+    switch cmd {
+    case "":
+        continue
+    default:
+        break
+    }
+    
+    if cmd == "exit" {
+        break
+    }
+    
+    let task = Process()
+    task.executableURL = URL(fileURLWithPath: cmd)
+    
+    do {
+        try task.run()
+    } catch CocoaError.fileNoSuchFile {
+        print("Bad command or file name")
+    }
+    
+    task.waitUntilExit()
+}
+
+print("Goodbye, World!")
